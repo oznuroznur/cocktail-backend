9e19262f7a4d609f1b34a1c5a8dbde5db0deb33b
Öznur Öznur <47983016+oznuroznur@users.noreply.github.com>
2025-08-04 16:36:03 +0300

    .

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT ? Number(process.env.PORT) : 8000;
 
 app.use(cors());
 app.use(express.json());
@@ -17,7 +17,6 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use('/api/cocktails', cocktailsRouter);
 
 
-
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running at http://0.0.0.0:${PORT}`);
 });
